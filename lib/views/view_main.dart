b68419d120127e796a3514e2ebceaedd1a7a6ecb
TaeBbong <mok05289@korea.ac.kr>
2024-10-18 18:38:01 +0900

    fix: update mainview with fontawesome

--- lib/views/view_main.dart
+++ lib/views/view_main.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../views/tabs/tab_counter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../views/tabs/tab_counter.dart';
 import '../viewmodels/controller_main.dart';
 
 class MainView extends StatelessWidget {
@@ -12,18 +15,10 @@ class MainView extends StatelessWidget {
     const Center(child: Text('Settings', style: TextStyle(fontSize: 24))),
   ];
 
-  static final List<String> _titles = <String>[
-    '바보 헬스 카운터',
-    '바보 헬스 기록',
-  ];
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-            title: Obx(
-                () => Text(_titles[bottomNavController.selectedIndex.value]))),
         body: Obx(
           () => _pages[bottomNavController.selectedIndex.value],
         ),
@@ -33,12 +28,12 @@ class MainView extends StatelessWidget {
             onTap: bottomNavController.changeTabIndex,
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
+                icon: FaIcon(FontAwesomeIcons.dumbbell),
+                label: '운동',
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                label: 'Settings',
+                icon: FaIcon(FontAwesomeIcons.clipboardCheck),
+                label: '기록',
               ),
             ],
           ),
